93f24b03e5af30e313e1f9ad3e3f9006aa7b8709
cpm0722 <cpm0722@kakao.com>
2020-08-25 23:41:39 +0900

    fin

--- start.sh
+++
@@ -1,2 +0,0 @@
-./smi2srt -nts -b /volume5/Video1/smi /volume5/Video1/drama_foreign /volume5/Video1/movie_foreign /volume5/Video1/movie_classic
-./smi2srt -b /volume6/Video2/smi /volume6/Video2/anime
