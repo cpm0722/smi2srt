8058738dcfe7d2d3222f751705303ccd04d9925a
Hansu Kim <cpm0722@gmail.com>
2021-01-10 00:52:27 +0900

    delete rename & scripts

--- scripts/many_volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-PWD=${0%/*}
-
-$PWD/smi2srt -nts -pwd $PWD -b /volume3/video2/smi_bak /volume3/video2/movie /volume3/video2/drama
-
-$PWD/smi2srt -nts -pwd $PWD -b /volume4/video3/smi_bak /volume4/video3/lecture
-
-$PWD/smi2srt -pwd $PWD /volume5/camera/2018 /volume5/camera/2019 /volume5/camera/2020
